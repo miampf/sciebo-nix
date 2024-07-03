161b0f997c4064d9a1f65c3cee06ec9b9747d0e3
miampf <miampf@proton.me>
2024-07-03 20:33:46 +0200

    finished package

--- flake.nix
+++ flake.nix
@@ -3,16 +3,19 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: 
-  let
-    pkgs = import nixpkgs {};
-  in
-  {
-    packages.sciebo-desktop = pkgs.callPackage ./sciebo-desktop.nix {};
-    overlays.default = self: super: {
-      sciebo-desktop = pkgs.callPackage ./sciebo-desktop.nix {};
-    };
-  };
+  outputs = { self, nixpkgs, flake-utils }:
+  flake-utils.lib.eachDefaultSystem (system: 
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      packages.sciebo-desktop = pkgs.callPackage ./sciebo-desktop.nix {};
+      overlays.default = self: super: {
+        sciebo-desktop = pkgs.callPackage ./sciebo-desktop.nix {};
+      };
+    }
+  );
 }
